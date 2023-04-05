1835adecd8ee48f708f21fb56490321f0388375e
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-05 08:50:40 +0530

    Added : first problem

--- src/com/classpractice/AmericanFlag.java
+++ src/com/classpractice/AmericanFlag.java
@@ -4,5 +4,12 @@ public class AmericanFlag {
 
     public static void main(String[] args) {
 
+        // Here I Take two variable s1,s2 for flag pattern;
+        String s1 = "* * * * * * =========================";
+        String s2 = " * * * * * ==========================";
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(s1);
+            System.out.println(s2);
+        }
     }
 }
